b03c4b603bf5d595396a1beb14c5c31369b03b15
DeBray Carpenter <debraycarpenter@DeBrays-Mac-mini.local>
2018-02-07 14:55:07 -0800

    initial setup of sample site

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const ejs = require('ejs');
+const path = require('path');
+var depLinker = require('dep-linker');
+
+// depLinker.setRoot('node_modules/bootstrap/dist')
+depLinker.linkDependenciesTo('./public/scripts')
+
+app.set('view engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/contact', (req, res) => {
+    res.render('contact')
+})
+
+app.get('/aboutus', (req, res) => {
+    res.render('aboutus')
+})
+
+app.listen(PORT, () => console.log(`APPLICATION LIVE ON ${PORT}`))
